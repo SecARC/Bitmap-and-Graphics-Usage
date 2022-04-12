f89b97631b8d7e0cde815a0eaaedb64323cf8e6d
Tuğra GÜNDÜZ <trgndz1999@gmail.com>
2022-04-12 19:16:59 +0300

    Drawing :)

---
+++ week03-3/week03-3/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿
+namespace week03_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.Heart = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number of Sides";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(146, 14);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(92, 26);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(10, 50);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(320, 320);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(436, 50);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(320, 320);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Heart
+            // 
+            this.Heart.AutoSize = true;
+            this.Heart.Location = new System.Drawing.Point(432, 16);
+            this.Heart.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Heart.Name = "Heart";
+            this.Heart.Size = new System.Drawing.Size(100, 20);
+            this.Heart.TabIndex = 4;
+            this.Heart.Text = "Heart Shape";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(677, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(79, 29);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Draw";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(765, 381);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Heart);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label Heart;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ week03-3/week03-3/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace week03_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            Bitmap bitmap = new Bitmap(300, 300);
+            Graphics g = Graphics.FromImage(bitmap);
+
+            g.Clear(Color.White);
+
+            //shape code will go here
+            int sides = (int)numericUpDown1.Value;
+
+            float r = 100;
+            PointF center = new PointF(150, 150);
+            float alpha = (float)(2 * Math.PI / sides);
+            float theta = 0;
+            //create a circle
+            g.DrawEllipse(Pens.Silver, center.X - r, center.Y - r, 2 * r, 2 * r);
+
+            //create multiple triangles until end of circle
+            while (theta < 2 * Math.PI)
+            {
+                PointF p0 = center;
+                PointF p1 = new PointF((float)(center.X + r * Math.Cos(theta)), (float)(center.Y + r * Math.Sin(theta)));
+                theta += alpha;
+                PointF p2 = new PointF((float)(center.X + r * Math.Cos(theta)), (float)(center.Y + r * Math.Sin(theta)));
+                string coord = string.Format("({0} {1})", Math.Round(p2.X - center.X, 1), -Math.Round((p2.Y - center.Y), 1));
+                g.DrawString(coord, SystemFonts.DefaultFont, Brushes.Blue, p2);
+
+                g.DrawPolygon(Pens.Purple, new PointF[] { p0, p1, p2 });
+            }
+            //write center point
+            g.DrawString("(0,0)", SystemFonts.DefaultFont, Brushes.Blue, center);
+
+
+            //end of shape code
+
+            bitmap.Save(@"C:\Users\PC\source\repos\week03-3\week03-3\images\polygon.jpg");
+            pictureBox1.Image = bitmap;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Bitmap bitmap = new Bitmap(300, 300);
+            Graphics g = Graphics.FromImage(bitmap);
+
+            g.Clear(Color.White);
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+
+            //shape code will go here
+
+            PointF center = new PointF(150, 150);
+            List<PointF> points = new List<PointF>();
+            float theta = 0;
+            float dt = (float)(2 * Math.PI / 100);
+            while (theta < 2 * Math.PI)
+            {
+                PointF p = new PointF(center.X + 5 * heartX(theta), center.Y - 5 * heartY(theta));
+                points.Add(p);
+                theta += dt;
+            }
+
+            g.FillPolygon(Brushes.Red, points.ToArray());
+
+
+            
+            //end of shape code
+
+            bitmap.Save(@"C:\Users\PC\source\repos\week03-3\week03-3\images\heart.jpg");
+            pictureBox2.Image = bitmap;
+        }
+        private float heartX(float theta)
+        {
+            return (float)(16 * (Math.Pow(Math.Sin(theta), 3)));
+        }
+        private float heartY(float theta)
+        {
+            return (float)(13 * Math.Cos(theta) - 5 * Math.Cos(2 * theta) - 2 * Math.Cos(3 * theta) - Math.Cos(4 * theta));
+        }
+    }
+}
